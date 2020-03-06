069b81266886385e9ebabad70419bbb032692b63
JBinggi <juerg.binggeli@gmail.com>
2020-03-07 00:04:48 +0100

    update

--- data/data_simple.sql
+++ data/data_simple.sql
@@ -1,5 +1,4 @@
 INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_list`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
-(NULL, 'datetime', 'Start', 'startdatetime', 'worktime-base', 'worktime-single', 'col-md-3', '', '', '0', '1', '0', '', '', ''),
-(NULL, 'datetime', 'Ende', 'enddatetime', 'worktime-base', 'worktime-single', 'col-md-3', '', '', '0', '1', '0', '', '', ''),
-(NULL, 'textarea', 'Kommentar', 'worktimecomment', 'worktime-base', 'worktime-single', 'col-md-12', '', '', '0', '1', '0', '', '', ''),
-(NULL, 'select', 'erfasster Benutzer', 'assigned_idfs', 'worktime-base', 'worktime-single', 'col-md-3', '', '/user/api/list/0', 0, 1, 0, 'user-single', 'OnePlace\\User\\Model\\UserTable','add-OnePlace\\User\\Controller\\UserController');
+(NULL, 'datetime', 'Start date', 'start_date', 'worktime-base', 'worktime-single', 'col-md-3', '', '', '0', '1', '0', '', '', ''),
+(NULL, 'datetime', 'End date', 'end_date', 'worktime-base', 'worktime-single', 'col-md-3', '', '', '0', '1', '0', '', '', ''),
+(NULL, 'textarea', 'Description', 'description', 'worktime-base', 'worktime-single', 'col-md-12', '', '', '0', '1', '0', '', '', '');
--- data/structure_simple.sql
+++ data/structure_simple.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `worktime`
-ADD `startdatetime` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `label`,
-ADD `enddatetime` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `startdatetime`,
-ADD `worktimecomment` TEXT NOT NULL DEFAULT '' AFTER `benutzer_idfs`,
-ADD `assigned_idfs` INT(11) NOT NULL DEFAULT '0' AFTER `worktimecomment`;
+ALTER TABLE `worktime` ADD `start_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `label`,
+ADD `end_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `start_date`,
+ADD `description` TEXT NOT NULL DEFAULT '' AFTER `end_date`;
